7995ac155e89bb848026558134b1b40b853823a8
Shamac3762 <shamac3762@hotmail.co.uk>
2026-03-17 22:31:23 +0000

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '100px', textAlign: 'center', background: '#111', color: 'white', minHeight: '100vh' }}>
+      <h1>Link Supply</h1>
+      <p>The smartest way to manage your NFC stickers.</p>
+      <a href="/login" style={{ color: '#0070f3' }}>Login to your Dashboard</a>
+    </div>
+  )
+}
