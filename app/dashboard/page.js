be3d7a571310efd330535e48f0fb315702eb62fa
Shamac3762 <shamac3762@hotmail.co.uk>
2026-03-18 09:31:47 +0000

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { createClient } from '@/utils/supabase/client'
+import { createClient } from '../../utils/supabase/client'
 
 export default function Dashboard() {
   const [stickers, setStickers] = useState([])
