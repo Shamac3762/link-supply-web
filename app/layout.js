d5458c4a4ce780b55de5c33f91923c8dcf6f54eb
Shamac3762 <shamac3762@hotmail.co.uk>
2026-05-11 14:19:51 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* 🔥 CookieYes Consent Banner */}
         <Script 
           id="cookieyes" 
-          src=<script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/a79521861388a137a5eb727b0e0233b6/script.js"></script>
-          strategy="beforeInteractive" 
+          src="PASTE_YOUR_URL_HERE" 
+          strategy="afterInteractive" 
         />
       </head>
       <body>{children}</body>
